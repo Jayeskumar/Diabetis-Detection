cfaa0f48bd5a7e68e2901cabe30a087f4678c674
Jayeskumar <52567903+Jayeskumar@users.noreply.github.com>
2020-12-15 21:49:02 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,9 +1,11 @@
+
 import numpy as np
 import pandas as pd
 from sklearn import svm
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report
+print("Hi Iam Jayes , and I am your persnal doctor !!!! Lets we take a Diabetis test")
 
 column_names = ["pregnancies", "glucose", "bpressure", "skinfold", "insulin", "bmi", "pedigree", "age", "class"]
 df = pd.read_csv("data.csv", names=column_names)
